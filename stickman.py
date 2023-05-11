18826ee1ff3134478c2c45300be791a665c7dbd1
ndeleche <ndelechehamis@gmail.com>
2023-05-11 15:55:08 -0700

    Update stickman.py

--- stickman.py
+++ stickman.py
@@ -3,6 +3,7 @@ import pyautogui as bot
 import random
 import keyboard
 import time
+import win32api, win32con
 
 images = {
     'blackfriday-1.png': 'Type 1',
@@ -11,21 +12,14 @@ images = {
     'blackfriday-4.png': 'Type 4'
 }
 
-time_to_wait = 600  # in seconds, i.e. 10 minutes
-last_detection_time = time.time() - time_to_wait  # so it will trigger the first time
-mouse_speed = 10  # adjust as needed
+time_to_wait = 300 # in seconds, i.e. 5 minutes
+last_detection_time = time.time() - time_to_wait # so it will trigger the first time
 
 while True:
     current_time = time.time()
     if current_time - last_detection_time >= time_to_wait:
         last_detection_time = current_time
         print('Starting new search...')
-        for i in range(3):  # move the mouse around randomly to simulate human use
-            x, y = position()
-            x += random.randint(-mouse_speed, mouse_speed)
-            y += random.randint(-mouse_speed, mouse_speed)
-            moveTo(x, y, duration=random.uniform(0.5, 1.5))
-            time.sleep(random.uniform(0.5, 1.5))
 
     images_to_detect = images.copy()
     while images_to_detect:
@@ -37,9 +31,7 @@ while True:
                 print(f'Center position of {image_type} image is {center}')
                 region = (center[0]-50, center[1]-50, 100, 100)
                 print(f'Region of {image_type} image is {region}')
-                bot.moveTo(center[0] + random.randint(-50, 50), center[1] + random.randint(-50, 50),
-                            duration=random.uniform(0.5, 1.5))  # move the mouse around randomly to simulate human use
-                time.sleep(random.uniform(0.5, 1.5))
+                bot.moveTo(center[0] + random.randint(-50, 50), center[1] + random.randint(-50, 50))
                 images_to_detect.pop(filename)
                 break
         else:
@@ -47,12 +39,3 @@ while True:
             break
 
     time.sleep(0.5)
-
-    # check for keyboard input to stop/start the bot
-    if keyboard.is_pressed('q'):
-        print('Quitting...')
-        break
-    elif keyboard.is_pressed('w'):
-        print('Starting new search...')
-        last_detection_time = time.time() - time_to_wait  # trigger a new search immediately
-        time.sleep(2)  # wait a bit to avoid accidentally triggering another search
